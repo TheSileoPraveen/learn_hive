f0cb43c45545166a54e605193cc3ea98d46a9f23
TheSileoPraveen <praveen@thesileo.com>
2025-07-22 12:04:43 +0530

    added splace screen.

---
+++ lib/splace_screen.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'note_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 5), () {
+      Get.offAll(NoteScreen()); // adjust route
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image.network(
+          height: 600,
+          width: 600,
+          "https://media0.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3bnZtMTZjNmlocDNlNG9odm1zY2tzZmlscXdzdGEwMWw3c3h0bTEwNyZlcD12MV9naWZzX3NlYXJjaCZjdD1n/PjCXGdk2BcUVGJp8p5/200.webp",
+        ),
+      ),
+    );
+  }
+}
